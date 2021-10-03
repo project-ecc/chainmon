c3d3f1336c739d4ff37920fbbbb9caa5f36f3d32
Henry Young <henry@home-young.me.uk>
2021-10-03 00:41:58 +0000

    Added settings

---
+++ settings.py
@@ -0,0 +1,24 @@
+# Blockchain Integration
+coin_symbol = 'ecc'
+rpc_address = '127.0.0.1:19119'
+rpc_user    = '<deleted>'
+rpc_pass    = '<deleted>'
+
+# Stall Detection
+stall_sec   = 10*60
+
+# Email Action
+email_enable  = True
+smtp_server   = "<deleted>"
+smtp_port     = 465 # For SSL
+email_pass    = "<deleted>"
+email_address = "<deleted>"
+send_email    = "<deleted>"
+
+# Twitter Action
+twitter_enable              = True
+twitter_api_key             = "<deleted>"
+twitter_api_secret_key      = "<deleted>"
+twitter_access_token        = "<deleted>"
+twitter_access_token_secret = "<deleted>"
+
